2141b1aac4231c8a1b76a7a4664ef95b9625ab36
Amit Gupta <121490421+amitgupta8@users.noreply.github.com>
2023-03-27 14:57:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,10 @@
-// Get references to the HTML elements
-const inputText = document.getElementById('text');
-const inputDelay = document.getElementById('delay');
-const button = document.getElementById('btn');
-const outputDiv = document.getElementById('output');
-
-// Define an async function that waits for a specified time before resolving with a message
-async function displayMessageAfterDelay(message, delay) {
-  await new Promise((resolve) => setTimeout(resolve, delay));
-  return message;
-}
-
-// Add an event listener to the button
-button.addEventListener('click', async () => {
-  // Get the values from the input fields
-  const message = inputText.value;
-  const delay = inputDelay.value;
-
-  // Call the async function to display the message after the delay
-  const result = await displayMessageAfterDelay(message, delay);
-
-  // Display the result in the output div
-  outputDiv.innerText = result;
-});
+async function displayTextAfterDelay(text, delay) {
+        await new Promise(resolve => setTimeout(resolve, delay));
+        document.getElementById("output").innerText = text;
+      }
+      
+      document.getElementById("btn").addEventListener("click", async () => {
+        const text = document.getElementById("text").value;
+        const delay = parseInt(document.getElementById("delay").value);
+        await displayTextAfterDelay(text, delay);
+      });
